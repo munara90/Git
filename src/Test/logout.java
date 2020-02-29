8809a06d71d52ea72ce836a309ccc75f3ac3c1d8
Munargul Shaiyrbekova <munara682@gmail.com>
2020-02-29 11:32:40 -0500

    update logout.java

--- src/Test/logout.java
+++ src/Test/logout.java
@@ -3,5 +3,7 @@ package Test;
 public class logout {
     public static void main(String[] args) {
         System.out.println("Hello Asya, we like your teaching style");
+
+        System.out.println("Lets open the git");
     }
 }
